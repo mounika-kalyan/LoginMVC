a26c52459beb1a0647d6ccaa61b9a67504654092
mounika-kalyan <60961792+mounika-kalyan@users.noreply.github.com>
2022-06-13 04:12:25 +0530

    Add project files.

---
+++ LoginMVC/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginMVC.Models;
+using System.Data.SqlClient;
+
+namespace LoginMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        //GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionSTring()
+        {
+            con.ConnectionString = "data source=MOUNIKAK; database=WPF; integrated security = SSPI;";
+        }
+        public ActionResult Verify(Account acc)
+        {
+            connectionSTring();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Login where UserName='"+acc.UserName+"' and Password='"+acc.Password+"' ";
+            dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ LoginMVC/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginMVC.Models
+{
+    public class Account
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
